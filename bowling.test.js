922a3bf01d86999502cb7d1b1571d4bb1c964047
balodi <balodi@gmail.com>
2018-01-23 10:25:21 +0530

    added 2nd testcase

--- bowling.test.js
+++ bowling.test.js
@@ -1,4 +1,7 @@
 let a = require('./bowling');
-test('this should return the score  : ',function () {
+test('this should return the score 0 : ',function () {
   expect(a([0,0,0,0,0,0,0,0,0,0])).toBe(0);
+});
+test('this should return the score  : ',function () {
+  expect(a([10,10,10,10,10,10,10,10,10,10,10,10])).toBe(300);
 });
